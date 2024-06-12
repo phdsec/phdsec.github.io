7fb4d059fc37aeb2cd90e005c2cde776fbcfb201
phdsec <mcy212001@myu.edu.pk>
2024-06-12 12:03:49 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -19,3 +19,8 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex';
+}
